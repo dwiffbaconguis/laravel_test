464c2e390ba2f9c9635d13afef50acb18ce49fa8
markquitos <mark_quitos@yahoo.com>
2015-01-14 17:07:13 +0800

    experimental

--- app/views/hello.php
+++ app/views/hello.php
@@ -5,9 +5,6 @@
 </head>
 <body>
 <<<<<<< HEAD
-	<p>Hello World! <br> test 2</p>
-=======
-	<p>Hello World! Branch Test One!!</p>
->>>>>>> f887cbe79276769f3b14966c8f00dc1e5450e156
+	<p>Hello World! <br> Experimental Branch</p>
 </body>
 </html>
