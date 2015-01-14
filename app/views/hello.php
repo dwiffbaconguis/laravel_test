7a65ebd8c37f6d5e6fb50c4971a40abc9d125c87
markquitos <mark_quitos@yahoo.com>
2015-01-14 16:57:16 +0800

    test

--- app/views/hello.php
+++ app/views/hello.php
@@ -4,6 +4,10 @@
 	<title>MDK Lido Loans</title>
 </head>
 <body>
+<<<<<<< HEAD
 	<p>Hello World! <br> test 2</p>
+=======
+	<p>Hello World! Branch Test One!!</p>
+>>>>>>> f887cbe79276769f3b14966c8f00dc1e5450e156
 </body>
 </html>
