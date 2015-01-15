1c43042bdcc1c0ce99ca19a03370cdcca6b1f587
Dwiff C. Baconguis <dwiff.baconguis@gmail.com>
2015-01-15 10:13:21 +0800

    from dwiff master branch

--- app/views/hello.php
+++ app/views/hello.php
@@ -4,13 +4,6 @@
 	<title>MDK Lido Loans</title>
 </head>
 <body>
-<p>dwiff master branch</p>
-<table>
-	<tr>
-		<td>1</td>
-		<td>1</td>
-		<td>1</td>
-	</tr>
-</table>
+	<p>dwiff master branch</p>
 </body>
 </html>
